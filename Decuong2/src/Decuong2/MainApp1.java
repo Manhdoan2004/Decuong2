0171d0f7c6893246d342896d0a5288efa9ac73f3
manh2004 <146045144+Manhdoan2004@users.noreply.github.com>
2023-12-25 14:37:58 +0700

    Add files via upload

---
+++ Decuong2/src/Decuong2/MainApp1.java
@@ -0,0 +1,28 @@
+package Decuong2;
+
+import java.util.Scanner;
+
+public class MainApp1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+
+		// Nhập giá trị n từ người dùng
+		System.out.print("Nhap gia tri n: ");
+		int n = scanner.nextInt();
+
+		// Tính tổng S
+		double tong = 0;
+		for (int i = 1; i <= n; i++) {
+			tong += 1.0 / (i * i);
+		}
+
+		// Hiển thị kết quả
+		System.out.println("Tong S la: " + tong);
+
+		// Đóng Scanner
+		scanner.close();
+	}
+
+}
